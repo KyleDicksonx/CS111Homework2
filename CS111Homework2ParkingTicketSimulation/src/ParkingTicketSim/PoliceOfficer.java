affae5f4f39ede690a69e217e9e4f72c80284457
redma <redma@10.10.23.110>
2025-04-27 18:03:20 -0700

    PoliceOfficer class is finished except for the ParkingTicket constructor (line 18) and comments

--- CS111Homework2ParkingTicketSimulation/src/ParkingTicketSim/PoliceOfficer.java
+++ CS111Homework2ParkingTicketSimulation/src/ParkingTicketSim/PoliceOfficer.java
@@ -1,5 +1,29 @@
 package ParkingTicketSim;
 
 public class PoliceOfficer {
-
+	private String name,badgeNumber;
+	
+	public PoliceOfficer(String name, String badgeNumber) {
+		this.name = name;
+		this.badgeNumber = badgeNumber;
+	}
+	
+	public PoliceOfficer(PoliceOfficer officer) {
+		this.name = officer.name;
+		this.badgeNumber = officer.badgeNumber;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setBadgeNumber(String badgeNumber) {
+		this.badgeNumber = badgeNumber;
+	}
+	public String getbadgeNumber() {
+		return this.badgeNumber;
+	}
 }
