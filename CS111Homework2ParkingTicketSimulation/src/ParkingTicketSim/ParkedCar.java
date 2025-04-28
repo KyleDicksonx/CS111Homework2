f70af8dd7d37c535633d822d21d608bcaf36b57a
redma <redma@10.10.23.110>
2025-04-28 15:00:25 -0700

    Added comments, removed equals method

--- CS111Homework2ParkingTicketSimulation/src/ParkingTicketSim/ParkedCar.java
+++ CS111Homework2ParkingTicketSimulation/src/ParkingTicketSim/ParkedCar.java
@@ -1,9 +1,18 @@
+/*
+ * Kyle Dickson
+ * 50116781
+ * I pledge that this submission is solely my work, and that I have neither given, nor received help from anyone.
+ */
+
 package ParkingTicketSim;
 
 public class ParkedCar {
+	
+	//fields
 	private String make,model,color,licensePlate;
 	private int minutesParked;
 	
+	//non-default constructor, passes make, model, color, license plate, and minutes parked
 	public ParkedCar(String make, String model, String color, String licensePlate, int minutesParked) {
 		this.make = make;
 		this.model = model;
@@ -12,6 +21,7 @@ public class ParkedCar {
 		this.minutesParked = minutesParked;
 	}
 	
+	//copy constructor
 	public ParkedCar(ParkedCar car) {
 		this.make = car.make;
 		this.model = car.model;
@@ -20,12 +30,14 @@ public class ParkedCar {
 		this.minutesParked = car.minutesParked;
 	}
 	
+	//toString method, returns all fields
 	public String toString() {
 		return "Make: " + this.make + "\nModel: " + this.model
 				+ "\nColor: " + this.color + "\nLicense Number: " + this.licensePlate
 					+ "\nMinutes Parked: " + this.minutesParked;
 	}
 	
+	//equals method to compare two strings //why did I make this red
 	public boolean equals(String thisInput, String compareInput) {
 		if (thisInput.equals(compareInput)) {
 			return true;
