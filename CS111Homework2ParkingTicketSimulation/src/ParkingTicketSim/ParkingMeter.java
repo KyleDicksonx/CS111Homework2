0ceb6558f171ffa32039c3a08e294fd428c54e1e
redma <redma@10.10.23.110>
2025-04-27 17:47:09 -0700

    Paking meter method finshed, no comments yet

--- CS111Homework2ParkingTicketSimulation/src/ParkingTicketSim/ParkingMeter.java
+++ CS111Homework2ParkingTicketSimulation/src/ParkingTicketSim/ParkingMeter.java
@@ -1,5 +1,20 @@
 package ParkingTicketSim;
 
 public class ParkingMeter {
-
+	private int minutesPurchased;
+	
+	public ParkingMeter(int minutesPurchased) {
+		this.minutesPurchased = minutesPurchased;
+	}
+	
+	public String toString() {
+		return "Minutes Purchased: " + this.minutesPurchased;
+	}
+	
+	public void setMinutesPurchased(int minutesPurchased) {
+		this.minutesPurchased = minutesPurchased;
+	}
+	public int getMinutesPurchased() {
+		return this.minutesPurchased;
+	}
 }
